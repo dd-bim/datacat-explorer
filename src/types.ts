ea5c96e808a4865d9cbe1403185fc7a05ebfe3b1
Benjamin Thurm <bentrm@posteo.de>
2020-04-14 15:32:33 +0200

    feat: Improve domain typing

--- src/types.ts
+++ src/types.ts
@@ -1,128 +1,150 @@
 type PageSize = 10 | 25 | 50 | 100;
 
 export interface PagingOptions {
-  pageSize?: number;
-  pageNumber?: number;
+    pageSize?: number;
+    pageNumber?: number;
 }
 
 export interface QueryArgs {
-  term?: string;
-  options?: PagingOptions;
+    term?: string;
+    options?: PagingOptions;
 }
 
 export interface Page {
-  pageNumber: number;
-  pageSize: PageSize;
-  totalPages: number;
-  totalElements: number;
+    pageNumber: number;
+    pageSize: PageSize;
+    totalPages: number;
+    totalElements: number;
 }
 
 export interface QueryConnection<T> {
-  nodes: T[]
-  page: Page
+    nodes: T[]
+    page: Page
 }
 
 export enum EntityTypes {
-  XtdLanguage = 'XtdLanguage',
-  XtdExternalDocument = 'XtdExternalDocument',
+    XtdLanguage = 'XtdLanguage',
+    XtdExternalDocument = 'XtdExternalDocument',
 }
 
 export enum XtdLanguageRepresentationTypes {
-  XtdName = 'XtdName',
-  XtdDescription = 'XtdDescription',
+    XtdName = 'XtdName',
+    XtdDescription = 'XtdDescription',
 }
 
 export enum XtdObjectTypes {
-  XtdActivity = 'XtdActivity',
-  XtdActor = 'XtdActor',
-  XtdClassification = 'XtdClassification',
-  XtdMeasureWithUnit = 'XtdMeasureWithUnit',
-  XtdProperty = 'XtdProperty',
-  XtdSubject = 'XtdSubject',
-  XtdUnit = 'XtdUnit',
-  XtdValue = 'XtdValue',
+    XtdActivity = 'XtdActivity',
+    XtdActor = 'XtdActor',
+    XtdClassification = 'XtdClassification',
+    XtdMeasureWithUnit = 'XtdMeasureWithUnit',
+    XtdProperty = 'XtdProperty',
+    XtdSubject = 'XtdSubject',
+    XtdUnit = 'XtdUnit',
+    XtdValue = 'XtdValue',
 }
 
 export enum XtdCollectionTypes {
-  XtdBag = 'XtdBag',
-  XtdNest = 'XtdNest',
+    XtdBag = 'XtdBag',
+    XtdNest = 'XtdNest',
 }
 
 export enum XtdRelationshipTypes {
-  XtdRelDocuments = 'XtdRelDocuments',
-  XtdAssociates = 'XtdAssociates',
-  XtdRelGroups = 'XtdRelGroups',
+    XtdRelDocuments = 'XtdRelDocuments',
+    XtdAssociates = 'XtdAssociates',
+    XtdRelGroups = 'XtdRelGroups',
 }
 
 export interface XtdLanguage {
-  __typename: EntityTypes.XtdLanguage;
-	id: string;
-	languageCode: string;
-	languageNameInEnglish: string;
-	languageNameInSelf: string;
+    __typename: EntityTypes.XtdLanguage;
+    id: string;
+    languageCode: string;
+    languageNameInEnglish: string;
+    languageNameInSelf: string;
 }
 
 export interface XtdLanguageRepresentation {
-  __typename: XtdLanguageRepresentationTypes;
-  id: string;
-  created: Date;
-  lastModified: Date;
-	languageCode: string;
-	languageName: XtdLanguage;
-	value: string;
+    __typename: XtdLanguageRepresentationTypes;
+    id: string;
+    created: Date;
+    lastModified: Date;
+    languageCode: string;
+    languageName: XtdLanguage;
+    value: string;
 }
 
 export interface XtdName extends XtdLanguageRepresentation {
-  __typename: XtdLanguageRepresentationTypes.XtdName;
+    __typename: XtdLanguageRepresentationTypes.XtdName;
 }
 
 export interface XtdDescription extends XtdLanguageRepresentation {
-  __typename: XtdLanguageRepresentationTypes.XtdDescription;
+    __typename: XtdLanguageRepresentationTypes.XtdDescription;
 }
 
 export interface Entity {
-  __typename: EntityTypes | XtdObjectTypes | XtdCollectionTypes | XtdRelationshipTypes;
-  id: string;
-  created: Date;
-  lastModified: Date;
+    __typename: EntityTypes | XtdObjectTypes | XtdCollectionTypes | XtdRelationshipTypes;
+    id: string;
+    created: Date;
+    lastModified: Date;
 }
 
 export interface XtdExternalDocument extends Entity {
-  __typename: EntityTypes.XtdExternalDocument;
-  names: XtdName[];
+    __typename: EntityTypes.XtdExternalDocument;
+    names: XtdName[];
 }
 
-export interface XtdRoot<T> extends Entity {
-  __typename: XtdObjectTypes | XtdCollectionTypes | XtdRelationshipTypes;
-	versionId: string;
-	versionDate: string;
-	label: string;
-	names: XtdName[]
-	descriptions: XtdDescription[]
-	groups: QueryConnection<T>
-	groupedBy: QueryConnection<T>
+export interface XtdRoot extends Entity {
+    __typename: XtdObjectTypes | XtdCollectionTypes | XtdRelationshipTypes;
+    versionId: string;
+    versionDate: string;
+    label: string;
+    names: XtdName[]
+    descriptions: XtdDescription[]
+    groups: QueryConnection<XtdRelGroups>
+    groupedBy: QueryConnection<XtdRelGroups>
 }
 
-export interface XtdObject<T> extends XtdRoot<T> {
-  __typename: XtdObjectTypes;
+export interface XtdObject extends XtdRoot {
+    __typename: XtdObjectTypes;
 }
 
-export interface XtdActivity extends XtdObject<XtdActivity> {
-  __typename: XtdObjectTypes.XtdActivity;
+export interface XtdActivity extends XtdObject {
+    __typename: XtdObjectTypes.XtdActivity;
 }
 
-export interface XtdActor extends XtdObject<XtdActor> {
-  __typename: XtdObjectTypes.XtdActor;
+export interface XtdActor extends XtdObject {
+    __typename: XtdObjectTypes.XtdActor;
 }
 
-export interface XtdSubject extends XtdObject<XtdSubject> {
-  __typename: XtdObjectTypes.XtdSubject;
+export interface XtdClassification extends XtdObject {
+    __typename: XtdObjectTypes.XtdClassification;
 }
 
-export interface XtdCollection<T> extends XtdRoot<T> {
-  __typename: XtdCollectionTypes;
+export interface XtdSubject extends XtdObject {
+    __typename: XtdObjectTypes.XtdSubject;
 }
 
-export interface XtdRelationship<T> extends XtdRoot<T> {
-  __typename: XtdRelationshipTypes;
+export interface XtdUnit extends XtdObject {
+    __typename: XtdObjectTypes.XtdUnit;
+}
+
+export interface XtdCollection extends XtdRoot {
+    __typename: XtdCollectionTypes;
+}
+
+export interface XtdBag extends XtdCollection {
+    __typename: XtdCollectionTypes;
+}
+
+export interface XtdNest extends XtdCollection {
+    __typename: XtdCollectionTypes;
+}
+
+export interface XtdRelationship extends XtdRoot {
+    __typename: XtdRelationshipTypes;
+}
+
+export interface XtdRelGroups extends XtdRelationship {
+    __typename: XtdRelationshipTypes;
+    relatingThing: XtdRoot
+    relatedThings: XtdRoot[]
 }
