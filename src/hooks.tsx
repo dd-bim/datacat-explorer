b107045ef332bcc98d836effb159203e6ed3fb6b
Benjamin Thurm <bentrm@posteo.de>
2020-04-16 08:47:20 +0200

    feat: Add custom useFindAllQuery hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import {QueryConnection, XtdEntity} from "./types";
+import {DocumentNode, QueryHookOptions, useQuery} from "@apollo/client";
 
 interface Pagination {
   pageNumber: number;
@@ -17,4 +19,13 @@ export function useQueryOptions(initalTerm = '',  initialPageNumber = 0, initial
   const [term, setTerm] = useState(initalTerm);
   const pagination = usePagination(initialPageNumber, initialPageSize);
   return { term, setTerm, ...pagination};
+}
+
+interface FindAllQueryData<T> {
+  [name: string]: QueryConnection<T>
+}
+
+export function useFindAllQuery<T extends XtdEntity>(query: DocumentNode, key: string, options: QueryHookOptions<FindAllQueryData<T>>) {
+  const { data, ...otherProps } = useQuery<FindAllQueryData<T>>(query, options);
+  return { nodes: data?.[key].nodes, page: data?.[key].page, ...otherProps };
 }
