a9c3b41b72b943a42c03886eedca5612edc1cb7f
Benjamin Thurm <bentrm@posteo.de>
2020-04-21 12:30:36 +0200

    feat: Add custom complex form value hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import {QueryConnection, XtdEntity} from "./types";
 import {DocumentNode, QueryHookOptions, useQuery} from "@apollo/client";
+import {useFormContext} from "react-hook-form";
+import get from "lodash.get";
 
 interface Pagination {
   pageNumber: number;
@@ -21,11 +23,33 @@ export function useQueryOptions(initalTerm = '',  initialPageNumber = 0, initial
   return { term, setTerm, ...pagination};
 }
 
-interface FindAllQueryData<T> {
+export interface FindOneQueryData<T> {
+  [name: string]: T
+}
+
+export function useFindOneQuery<T extends XtdEntity>(query: DocumentNode, key: string, options: QueryHookOptions<FindOneQueryData<T>>) {
+  const { data, ...otherProps } = useQuery<FindOneQueryData<T>>(query, options);
+  return { result: data?.[key], ...otherProps };
+}
+
+export interface FindAllQueryData<T> {
   [name: string]: QueryConnection<T>
 }
 
 export function useFindAllQuery<T extends XtdEntity>(query: DocumentNode, key: string, options: QueryHookOptions<FindAllQueryData<T>>) {
   const { data, ...otherProps } = useQuery<FindAllQueryData<T>>(query, options);
   return { nodes: data?.[key].nodes, page: data?.[key].page, ...otherProps };
+}
+
+export function useAsFormValue(options: { name: string, defaultValue: XtdEntity | null }) {
+  const { name, defaultValue } = options;
+  const { errors } = useFormContext();
+  const [current, setCurrent] = useState(defaultValue);
+  const error = get(errors, name);
+
+  const handleNewConcept = (concept?: XtdEntity) => {
+    setCurrent(concept ? concept : null);
+  };
+
+  return {current, setValue: handleNewConcept, error};
 }
