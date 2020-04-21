97174f8976a27c046cbcd99dabdb7291de1c8b3c
Benjamin Thurm <bentrm@posteo.de>
2020-04-21 17:21:27 +0200

    fix: Add missing routes

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,21 +2,32 @@ import React from 'react';
 import {EntityTypes, XtdCollectionTypes, XtdObjectTypes, XtdRelAssociatesTypes, XtdRelationshipTypes} from "./types";
 
 export const route = (typename: EntityTypes | XtdObjectTypes | XtdCollectionTypes | XtdRelationshipTypes | XtdRelAssociatesTypes): string => {
-  switch (typename) {
-    case XtdObjectTypes.XtdActivity:
-      return '/objects/activities';
-    case XtdObjectTypes.XtdActor:
-      return '/objects/actors';
-    case XtdObjectTypes.XtdSubject:
-      return '/objects/subjects';
-    case XtdCollectionTypes.XtdBag:
-      return '/collections/bags';
-    case XtdCollectionTypes.XtdNest:
-      return '/collections/nests';
-    case XtdRelAssociatesTypes.XtdRelGroups:
-      return '/relationships/groups';
-    default: throw new Error("Unknown type")
-  }
+    switch (typename) {
+        case XtdObjectTypes.XtdActivity:
+            return '/objects/activities';
+        case XtdObjectTypes.XtdActor:
+            return '/objects/actors';
+        case XtdObjectTypes.XtdSubject:
+            return '/objects/subjects';
+        case XtdObjectTypes.XtdUnit:
+            return '/objects/units';
+        case XtdObjectTypes.XtdProperty:
+            return '/objects/properties';
+        case XtdCollectionTypes.XtdBag:
+            return '/collections/bags';
+        case XtdCollectionTypes.XtdNest:
+            return '/collections/nests';
+        case XtdRelationshipTypes.XtdRelDocuments:
+            return '/relationships/documents';
+        case XtdRelationshipTypes.XtdRelCollects:
+            return '/relationships/collects';
+        case XtdRelationshipTypes.XtdRelAssociates:
+            return '/relationships/associates';
+        case XtdRelAssociatesTypes.XtdRelGroups:
+            return '/relationships/groups';
+        default:
+            throw new Error("Unknown type")
+    }
 };
 
 export const removeTypename = (obj: any) => {
@@ -31,7 +42,7 @@ export const removeTypename = (obj: any) => {
     return obj;
 };
 
-export const sanitizeTextInput = (x: {id: string | null}) => {
+export const sanitizeTextInput = (x: { id: string | null }) => {
     x.id = x.id === "" ? null : x.id;
 };
 
