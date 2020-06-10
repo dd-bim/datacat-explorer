d2f8c9dc39b915fe0390aa6d89792a3c55c9a7e6
Benjamin Thurm <bentrm@posteo.de>
2020-06-10 10:18:05 +0200

    feat: Add general form field options

---
+++ src/components/form/TextFieldOptions.ts
@@ -0,0 +1,10 @@
+import {TextFieldProps} from "@material-ui/core";
+
+const TextFieldOptions: TextFieldProps = {
+    fullWidth: true,
+    margin: 'dense',
+    size: 'medium',
+    variant: 'outlined',
+}
+
+export default TextFieldOptions;
