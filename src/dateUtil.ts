c7cd9fc16cde8e860ff38a0a09aeef0787c2382f
Benjamin Thurm <bentrm@posteo.de>
2020-06-22 08:26:59 +0200

    feat: Add relative time plugin to date time util

--- src/dateUtil.ts
+++ src/dateUtil.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import de from 'dayjs/locale/de';
 
 dayjs.locale(de);
 dayjs.extend(LocalizedFormat);
+dayjs.extend(relativeTime);
 
 export default dayjs;
